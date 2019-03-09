36f74221e367c32c2c0c0fba9d8521fbaa72a989
sawan5216977 <sawansingh5216977@gmail.com>
2019-03-10 02:25:01 +0530

    display group messages on screen & fixes some minor bugs

--- app/src/main/java/com/example/lageraho/GroupChatActivity.java
+++ app/src/main/java/com/example/lageraho/GroupChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.lageraho;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +24,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class GroupChatActivity extends AppCompatActivity {
 
@@ -74,8 +77,74 @@ public class GroupChatActivity extends AppCompatActivity {
 
                 messageInputEditText.setText("");  // clear the message box after sending the message...
 
+                // it will automatically scroll down to the new message whenever it is going to be send.
+                mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        groupDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                if (dataSnapshot.exists()){
+                    displayMessages(dataSnapshot);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                if (dataSnapshot.exists()){
+                    displayMessages(dataSnapshot);
+                }
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void displayMessages(DataSnapshot dataSnapshot) {
+
+        // iterate for each children in each message key
+        Iterator iterator = dataSnapshot.getChildren().iterator();
+
+        while (iterator.hasNext()){
+
+            // all these four variables must be in same order in which order the data is saved in FireBase Database.
+            String chatName = (String) ((DataSnapshot) iterator.next()).getValue();
+            String chatMessage = (String) ((DataSnapshot) iterator.next()).getValue();
+            String chatDate = (String) ((DataSnapshot) iterator.next()).getValue();
+            String chatTime = (String) ((DataSnapshot) iterator.next()).getValue();
+
+            // append or add the data and display it on the Text View
+            mDisplayMessage.append(chatName + " :\n" + chatMessage + "\n" + chatTime + "   " + chatDate + "\n\n\n");
+
+            // it will automatically scroll down to the new message whenever it is going to be send.
+            mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+
+        }
 
     }
 
